7a969974097e9c9892f9a5a1015b400d6b46cfc0
JJJoonngg <whdtls3878@gmail.com>
2021-05-23 16:03:03 +0900

    #5 kakao login 구현
    - 카카오 톡이 존재하면 카카오톡으로, 존재하지 않는다면 이를 카카오 계정을 통해서 진행하도록 수행
    - 유저의 토큰값을 이용하여 회원 여부를 체크 예정

--- NanDoChak/app/src/main/java/kr/co/west_gang/nan_do_chak/view/login/LoginActivity.kt
+++ NanDoChak/app/src/main/java/kr/co/west_gang/nan_do_chak/view/login/LoginActivity.kt
@@ -1,15 +1,18 @@
 package kr.co.west_gang.nan_do_chak.view.login
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import kr.co.west_gang.nan_do_chak.R
 import kr.co.west_gang.nan_do_chak.architecture.BaseActivity
 import kr.co.west_gang.nan_do_chak.databinding.ActivityLoginBinding
 import kr.co.west_gang.nan_do_chak.util.AppConfig.TAG_DEBUG
 import kr.co.west_gang.nan_do_chak.util.logD
+import kr.co.west_gang.nan_do_chak.view.main.MainActivity
 
 class LoginActivity : BaseActivity() {
 
@@ -22,28 +25,63 @@ class LoginActivity : BaseActivity() {
         binding.viewModel = viewModel
 
         observeLiveData()
+
+        val keyHash = Utility.getKeyHash(this)
+        logD("Hash", "keyHash : $keyHash")
     }
 
     private fun observeLiveData() {
-        viewModel.isLoginButtonClicked.observe(this, Observer { isClicked ->
-            if (isClicked) {
-                val isKakaoTalkLoginAvailable =
-                    UserApiClient.instance.isKakaoTalkLoginAvailable(this)
+        with(viewModel) {
+            isLoginButtonClicked.observe(this@LoginActivity, Observer { isClicked ->
+                loginSessionCheck(isClicked)
+            })
 
-                if (isKakaoTalkLoginAvailable) {
-                    UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-                        if (error != null) {
-                            logD(TAG_DEBUG, "로그인 실패 $error")
-                        } else if (token != null) {
-                            logD(TAG_DEBUG, "로그인 성공 ${token.accessToken}")
-                        }
-                    }
-                } else {
-                    Toast.makeText(this, "KaKao Talk 미설치 ", Toast.LENGTH_SHORT).show()
+            isSignUpButtonClicked.observe(this@LoginActivity, Observer { isClicked ->
+                if (isClicked) {
+                    startSignUpActivity()
                 }
-            }
-
-        })
+            })
+        }
     }
 
+    private fun loginSessionCheck(isClicked: Boolean) {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                //Login Fail
+                logD(TAG_DEBUG, "Login Fail : $error")
+                //TODO : login 실패 모달 창
+            } else if (token != null) {
+                //Login Success
+                logD(TAG_DEBUG, "token : ${token.accessToken}")
+                //TODO : 회원 정보 확인
+                //token 을 이용해서 firebase 에 유저가 존재하는지 check
+                startMainActivity()
+            }
+        }
+        if (isClicked) {
+            val isKakaoTalkLoginAvailable =
+                UserApiClient.instance.isKakaoTalkLoginAvailable(this)
+
+            if (isKakaoTalkLoginAvailable) {
+                UserApiClient.instance.loginWithKakaoTalk(
+                    context = this,
+                    callback = callback
+                )
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(
+                    context = this,
+                    callback = callback
+                )
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun startSignUpActivity() {
+        //TODO : 회원 가입 로직 진행
+    }
 }
--- NanDoChak/app/src/main/java/kr/co/west_gang/nan_do_chak/view/login/LoginViewModel.kt
+++ NanDoChak/app/src/main/java/kr/co/west_gang/nan_do_chak/view/login/LoginViewModel.kt
@@ -16,7 +16,15 @@ class LoginViewModel : ViewModel() {
     private val _isLoginButtonClicked: MutableLiveData<Boolean> = MutableLiveData(false)
     val isLoginButtonClicked: LiveData<Boolean> = _isLoginButtonClicked
 
+    private val _isSignUpButtonClicked: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isSignUpButtonClicked: MutableLiveData<Boolean> = _isSignUpButtonClicked
+
+
     fun loginButtonClicked() {
         _isLoginButtonClicked.value = true
+    }
+
+    fun signUpButtonClicked() {
+        _isSignUpButtonClicked.value = true
     }
 }
